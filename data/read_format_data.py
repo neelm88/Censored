8932bd15db8cfefaee1299d0fe4784cbb002f8e7
sriramyadandu <sriramyadandu@yahoo.com>
2021-03-31 12:32:58 -0400

    remove extra spaces and comma

--- data/read_format_data.py
+++ data/read_format_data.py
@@ -6,7 +6,9 @@ http_regex = '(http|ftp|https)(:\/\/)([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+
 rt_regex = "RT"
 username_regex = '@[a-zA-Z0-9_]*'
 decimal_rep_regex = '&#[0-9]*|&amp;' # For &#.... or $amp;
-special_char_regex = '[^A-Za-z0-9 ,]'
+special_char_regex = '[^A-Za-z0-9 ]'
 combined_regex = '|'.join((http_regex,rt_regex,username_regex,decimal_rep_regex,special_char_regex))
 df['tweet']=  [re.sub(combined_regex,'', str(text)) for text in df['tweet']]
+space_regex = "\\s+"
+df['tweet']=  [re.sub(space_regex,' ', str(text)) for text in df['tweet']]
 df.to_csv('labeled_data.csv', index=False)
